08e1c8bee0064f9b260dd3a7bcef9e3632cba05c
1208kelly <49724332+1208kelly@users.noreply.github.com>
2020-06-18 16:28:13 +0100

    Adding button commands to js/index.js

--- js/index.js
+++ js/index.js
@@ -4,4 +4,48 @@ let lightUpOrder = [];
 
 let lightFlash = [];
 
-let roundCounter =
+let roundCounter = document.querySelector("#round-counter");
+
+let redButton = document.querySelector("#red-button");
+
+let blueButton = document.querySelector("#blue-button");
+
+let yellowButton = document.querySelector("#yellow-button");
+
+let greenButton = document.querySelector("#green-button");
+
+let startButton = document.querySelector("#start-button");
+
+startButton.addEventListener("click", function(event) {
+    if (startButton.clicked === true) {
+        on = true;
+        roundCounter.innerHTML = "-";
+        playGame();
+    } else {
+        on = false;
+        roundCounter.innerHTML = "";
+        lightsOff();
+    }
+});
+
+function playGame() {
+    win = false;
+    lightUpOrder = [];
+    lightFlash = 0;
+    turn = 1;
+    roundCounter.innerHTML = 1;
+    for (var i = 0; i < 20; i++) {
+        lightUpOrder.push(Math.floor(Math.random() * 4) + 1);
+    }
+    compTurn = true;
+
+    intervalId = setInterval(gameTurn, 800);
+    console.log(lightUpOrder);
+};
+
+function lightsOff() {
+    redButton.style.backgroundColor = "darkred";
+    blueButton.style.backgroundColor = "darkblue";
+    yellowButton.style.backgroundColor = "goldenrod";
+    greenButton.style.backgroundColor = "darkgreen";
+};
