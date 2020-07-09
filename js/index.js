f9c731126f52521758a0dff49dd7251a1055cae4
1208kelly <49724332+1208kelly@users.noreply.github.com>
2020-07-09 14:43:30 +0100

    Updating button light flash

--- js/index.js
+++ js/index.js
@@ -68,7 +68,7 @@ function playGame() {
     roundCounter.innerHTML = "1";
     intervalId = 0;
     playerTurn = 1;
-    lightflash = 0;
+    lightFlash = 0;
     good = true;
     gameTurn = true;
     intervalId = setInterval(gameRound, 1000);
